e05ac6d1d51a76b841c7e90b962ea989d90d0ced
ApplePie <IMustNotHaveFun@gmail.com>
2023-12-04 14:36:23 +0100

    Day 4 solution complete

--- 2023/day_4/Scratchcards.py
+++ 2023/day_4/Scratchcards.py
@@ -1,47 +1,37 @@
-
-
-# what should I know 
-# 
-
+# cpode inspired by user's FlyingPlatypus5 solution on https://www.reddit.com/r/adventofcode/comments/18actmy/2023_day_4_solutions/
 
 def main():
 
-    cardSum = 0
+    with open("example1.txt") as usr_input:
+        text = usr_input.readlines()
 
-    with open("puzzle_input.txt", "r") as usrInput:
-        for line in usrInput:
-            cardSum += scratchCardCounter(line)
-    
-    #print()
-    print(cardSum)
+        numSum = 0
+        cards = [1 for _ in text]
+        print(cards)
+
+        for index, line in enumerate(text):
+
+            line = line.split(":")[1].replace("\n","")
+
+            win_nums, your_nums = line.split("|")
+            win_nums, your_nums = win_nums.split(), your_nums.split()
+
+            winning_numbers = len((set(win_nums) & set(your_nums)))
+            print(winning_numbers)
+
+            if winning_numbers > 0:
+                numSum += 1 * 2**(winning_numbers-1)
+
+
+            for i in range(winning_numbers):
+                cards[index + i + 1] += cards[index]
+                print(cards)
+
+        print(numSum, sum(cards))
 
 
 
-def scratchCardCounter(line):
 
-    cardNum, number = line.split(":")
-    winNum, yourNum = number.split("|")
-
-    winNumList = winNum.strip().replace("\n", "").replace("  ", " ").split(" ")
-    yourNumList = yourNum.strip().replace("\n","").replace("  ", " ").split(" ")
-
-    winCounter = 0
-    #print(f"winNumList: {winNumList}")
-    #print(f"youNumList: {yourNumList}")
-    for i in yourNumList:
-        if i in winNumList:
-            winCounter += 1
-        else:
-            continue
-
-    if winCounter >= 1:
-        gameCounter = 1 * 2**(winCounter-1)
-    else:
-        gameCounter = 0
-
-    #print(gameCounter)
-    return gameCounter
-    
 
 if __name__ == "__main__":
     main()
